92629db9020e169a9f763a771e927a28a5dc0fad
dodonmountain <lkh151515@gmail.com>
2020-03-31 01:02:01 +0900

    kh | make prototype of Home.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,14 +5,18 @@ import { Header } from './components';
 import { Home, About } from './pages'
 
 function App() {
+
   const user = {name:'Jack', age: 32}
+
   return (
     <div className="App">
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      {JSON.stringify(user)}
-
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <div className="container center">
+          {JSON.stringify(user)}
+        </div>
+        
     </div>
   );
 }
