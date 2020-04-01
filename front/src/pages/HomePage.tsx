a49d219c9e2779f9340c4df041c5c3e099fcf888
dodonmountain <lkh151515@gmail.com>
2020-04-01 15:23:52 +0900

    kh | add redux

---
+++ front/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Header, Slider } from '../components'
+import { Link } from 'react-router-dom'
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <Slider />
+      <div className="center">
+        <Link to="/login">로그인 페이지로 가기</Link>
+      </div>
+      <h1 className="center">대충 카드를 넣고싶은 곳</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ front/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AccessAlarm } from '@material-ui/icons';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1 className="center"><AccessAlarm />로그인 페이지</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
