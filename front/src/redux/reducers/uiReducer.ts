8448766be89887626fcac95fef94accab273186b
dodonmountain <lkh151515@gmail.com>
2020-04-02 09:08:54 +0900

    kh | added reducers

--- front/src/redux/reducers/uiReducer.ts
+++ front/src/redux/reducers/uiReducer.ts
@@ -7,19 +7,19 @@ export default function (state = initialState, action: any) {
   switch (action.type) {
     case SET_ERRORS:
       return {
-        …state,
+        ...state,
         loading: false,
         errors: action.payload
       };
     case CLEAR_ERRORS:
       return {
-        …state,
+        ...state,
         loading: false,
         errors: null
       };
     case LOADING_UI:
       return {
-        …state,
+        ...state,
         loading: true
       }
     default:
--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -4,15 +4,16 @@ import userReducer from './reducers/userReducer'
 import uiReducer from './reducers/uiReducer'
 const initialState = {};
 const middleware = [thunk];
-//this is for redux devtool purpose
+
+// 리덕스 개발자 도구 지원용 인터페이스
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
   }
 }
 const reducer = combineReducers({
-  user: userReducer,//user key ma store gareko
+  user: userReducer, // 유저 키
   UI: uiReducer
 });
-const store = createStore(reducer, initialState, compose(applyMiddleware(…middleware), (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) as any));
+const store = createStore(reducer, initialState, compose(applyMiddleware(...middleware), (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) as any));
 export default store;
