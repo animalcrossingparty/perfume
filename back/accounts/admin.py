05345da8556215f6ed5487cb6b5c15c08fa96fd4
HyejinYang <yang94lol@naver.com>
2020-04-05 22:57:13 +0900

    hj | Init

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 
-admin.site.register(get_user_model())
+# Register your models here.
--- back/perfume/urls.py
+++ back/perfume/urls.py
@@ -14,34 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
-
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Accounts API",
-      default_version='v1',
-      description="User Info & Similarity Between Users"
-   ),
-)
-
-BASE_PATH = 'api/v1/'
-
-def home(request):
-    return redirect(f'{BASE_PATH}swagger/')
+from django.urls import path
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
-    path(f'{BASE_PATH}accounts/', include('accounts.urls')),
-    # jwt-token
-    path(f'{BASE_PATH}token/', obtain_jwt_token),
-    path(f'{BASE_PATH}token/verify/', verify_jwt_token),
-    path(f'{BASE_PATH}token/refresh/', refresh_jwt_token),
-    # path('redoc/', schema_view.with_ui('redoc'), name="api_docs"),
-    path(f'{BASE_PATH}swagger/', schema_view.with_ui('swagger'), name="api_swagger"),
 ]
