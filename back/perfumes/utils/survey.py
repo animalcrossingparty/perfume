703662c3ed15b491c83865eef97dcae3451fca3c
박홍은 <tkfkd1909@naver.com>
2020-04-16 13:24:26 +0900

    he | EDIT models & survey.py S02P23B208-18

--- back/perfumes/utils/survey.py
+++ back/perfumes/utils/survey.py
@@ -1,8 +1,11 @@
 import json
 import pandas as pd
-import perfumes.models import Perfume
+from perfumes.models import Perfume, Note, Brand
+from django.contrib.auth import get_user_model
+from accounts.models import User
+from django.db.models import Q
 
-def selected_perfumes(perfumes):
+def selected_perfumes(survey):
     """
     참고로 독스트링은 스웨거에 나오니까 예쁘게 쓰면 좋습니다. drf_yasg 문서에 독스트링 쓰는법 나와있으
     {
@@ -14,18 +17,26 @@ def selected_perfumes(perfumes):
         "notes": ARRAY
     }
     """
-    "객 체 지 향 하지만 이 속도로 작성해서는 12일안에 아무것도 만들 수가 없는걸..."
-    
-    perfumes[0]
+    gender = survey[0]['gender']
+    age = survey[0]['age']
+    season = survey[0]['season']
+    hates = survey[0]['hates']
+    likes = survey[0]['likes']
+    category = survey[0]['notes']
 
+    perfumes = Perfume.objects.filter(Q(gender=gender) | Q(gender__isnull=True) , season__in=season)
+    # notes = Note.objects.filter(name__contains=survey[0]['notes'])
+    if hates:
+      perfumes = perfumes.exclude(Q(top_notes__in=hates) | Q(heart_notes__in=hates) | Q(base_notes__in=hates))
+    
+    return []
 if __name__ == "__main__":
-  perfumes = [{
+  survey = [{
     "gender": "male",
-    "age": 239,
-    "season": "winter",
+    "age": 23,
+    "season": [0, 1, 2, 3], # 사계절
     "likes": ["citrus", "musk", "mint"],
     "hates": ["wood", "kimchi"],
     "notes": ["citrus", "kimchi", "cat", ""]
-    ""
   }]
-  selected_perfumes(perfumes)
+  selected_perfumes(survey)
