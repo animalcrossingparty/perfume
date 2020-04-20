4098196b263cd5d0fd8086ea305db5213cc799ee
dodonmountain <lkh151515@gmail.com>
2020-04-20 16:32:48 +0900

    kh | FIX cors whitelist

--- back/laure_richis/production.py
+++ back/laure_richis/production.py
@@ -13,6 +13,8 @@ DATABASES = {
 STATIC_DIR = os.path.join(BASE_DIR, 'code', 'static')
 DEBUG = True
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000/'
-)
+CORS_ORIGIN_WHITELIST = [
+    # 배포 주소 추가 예정
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
