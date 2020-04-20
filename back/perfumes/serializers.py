e6ca8bd83e65215dae4af92fa07b4e9281e10aca
HyejinYang <yang94lol@naver.com>
2020-04-20 15:26:55 +0900

    hj | TEMP review apis

--- back/perfumes/serializers.py
+++ back/perfumes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Perfume
+from accounts.serializers import UserSerializers
 
 
 class PerfumeSerializers(serializers.ModelSerializer):
@@ -7,7 +8,15 @@ class PerfumeSerializers(serializers.ModelSerializer):
         model = Perfume
         fields = '__all__'
 
+class ReviewDetailSerializers(serializers.Serializer):
+    user = serializers.IntegerField(source='user.pk')
+    perfume = serializers.IntegerField(source='perfume.pk')
+    content = serializers.CharField()
+    rate = serializers.IntegerField(min_value=0, max_value=10)
+    created_at = serializers.DateTimeField()
+
 class PerfumeDetailSerializers(serializers.ModelSerializer):
+    reviews = ReviewDetailSerializers(many=True, source='review_set')
     class Meta:
         model = Perfume
         fields = '__all__'
--- back/perfumes/views.py
+++ back/perfumes/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Perfume, Review
-from .serializers import PerfumeSerializers, PerfumeDetailSerializers
+from .serializers import PerfumeSerializers, PerfumeDetailSerializers, ReviewDetailSerializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
+import jwt
+from time import time
+from django.contrib.auth import get_user_model
 
 @api_view(['GET'])
 def perfumes_list(request):
@@ -14,14 +18,50 @@ def perfumes_list(request):
     print(serializer)
     return Response(serializer.data)
 
+@api_view(['GET'])
 def perfume_detail(request, perfume_pk):
-    pass
+    perfume = Perfume.objects.get(pk=perfume_pk)
+    serializer = PerfumeDetailSerializers(perfume)
+    return Response(serializer.data)
 
 def perfume_survey(request):
     pass
 
-def reviews_list(request, perfume_pk):
-    pass
 
+@api_view(['POST'])
+def review_create(request, perfume_pk):
+    encoded_jwt = request.headers['Token']
+    try:
+        decoded = jwt.decode(encoded_jwt, settings.SECRET_KEY, algorithms=['HS256'])
+        user = get_user_model().objects.get(pk=decoded['userID'])
+    except:
+        return Response(status=401)
+    else:
+        data = request.body
+        print(data)
+        # Review.objects.create(
+        #     user=user,
+        #     perfume=perfume_pk,
+        #     content=
+        # )
+        return Response(status=200)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def review_detail(request, perfume_pk, review_pk):
-    pass
+    if request.method == 'GET':
+        try:
+            review = Review.objects.get(pk=review_pk)
+            serializer = ReviewDetailSerializers(review)
+            return Response(serializer.data)
+        except:
+            return Response(status=404)
+
+
+
+
+
+
+
+
+
+# {'token': b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySUQiOjEsInVzZXJuYW1lIjoiYW5vbnltb3VzMUB0ZXN0LmNvbSIsImlhdCI6MTU4NzM1NjI0MSwiZXhwIjoxNTk0NTU2MjQxfQ.NLFscFfepnNGJaPcSfpYBMuj0Ciyt3j89RDOf6J96Xg'}
