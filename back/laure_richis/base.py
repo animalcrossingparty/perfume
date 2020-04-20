5e56d9972b88f93ccec5ceb587d73d72d0d6d10e
dodonmountain <lkh151515@gmail.com>
2020-04-20 15:29:47 +0900

    kh | IMPLEMENT CORS header on django

--- back/laure_richis/base.py
+++ back/laure_richis/base.py
@@ -29,9 +29,11 @@ INSTALLED_APPS = [
     'drf_yasg',
     'accounts',
     'perfumes',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
