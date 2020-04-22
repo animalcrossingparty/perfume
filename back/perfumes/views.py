785ea302ec116938906cb09c59ac4e837b4cf5ae
박홍은 <tkfkd1909@naver.com>
2020-04-22 11:36:09 +0900

    he | EDIT perfume_list

--- back/perfumes/views.py
+++ back/perfumes/views.py
@@ -35,28 +35,28 @@ def perfumes_list(request):
 
     # 성별 체크
     print(gender)
-    if gender is not 'all':
+    if gender != 'all':
         try:
             products = products.filter(gender=gender)
         except:
             return 0
 
     # 브랜드 체크
-    if brand is not 'all':
+    if brand != 'all':
         try:
             products = products.filter(brand__name=brand)
         except:
             return 0
 
     # 카테고리 체크
-    if category is not 'all':
+    if category != 'all':
         try:
             products = products.filter(categories=category)
         except:
             return 0
 
     # 제외 노트 체크
-    if exclude is not None:
+    if exclude != None:
         try:
             exclude_list = exclude.split(',')
             for exclude in exclude_list:
@@ -66,7 +66,7 @@ def perfumes_list(request):
             return 0
 
     # 포함 노트 체크
-    if include is not 'all':
+    if include != 'all':
         try:
             include_list = include.split(',')
             for include in include_list:
@@ -256,11 +256,11 @@ def perfume_survey(request):
 
 
 
-# def make_wordcloud(request, perfume_pk):
-#     reviews = Review.objects.filter(perfume=perfume_pk)
-#     wordcloud.tokenizing(reviews)
+def make_wordcloud(request, perfume_pk):
+    reviews = Review.objects.filter(perfume=perfume_pk)
+    wordcloud.tokenizing(reviews)
 
-#     serializer = PerfumeSerializers()
+    serializer = PerfumeSerializers()
     
 
 
