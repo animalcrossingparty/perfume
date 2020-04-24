2f2f5e206ccaac8e4c78620125de8adc637608df
dodonmountain <lkh151515@gmail.com>
2020-04-24 17:02:33 +0900

    kh | add front build

---
+++ front/build/precache-manifest.85ade27ec397f8ae913ae929fe37b07f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c172f863aacb580527a707342df318e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798eeb1ec96e0f457af0",
+    "url": "/static/css/2.9b46dffb.chunk.css"
+  },
+  {
+    "revision": "965ee24f2b00f659f9fe",
+    "url": "/static/css/main.d99103dc.chunk.css"
+  },
+  {
+    "revision": "798eeb1ec96e0f457af0",
+    "url": "/static/js/2.1106ecb3.chunk.js"
+  },
+  {
+    "revision": "2c25ac50775cbac61507fa8fb422f472",
+    "url": "/static/js/2.1106ecb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965ee24f2b00f659f9fe",
+    "url": "/static/js/main.ae4a18f3.chunk.js"
+  },
+  {
+    "revision": "2f679a56ecee95c97a53",
+    "url": "/static/js/runtime-main.8bdef2b8.js"
+  },
+  {
+    "revision": "a052fc8845f407a6eab04a6ca4da213c",
+    "url": "/static/media/detail-bg.a052fc88.png"
+  },
+  {
+    "revision": "11ddf8dce35a90788cb3397bb258b3bf",
+    "url": "/static/media/detail-bg2.11ddf8dc.png"
+  }
+]);
