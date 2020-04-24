4627ff1ed24e4d300e70c871b38cdae2a82fd397
dodonmountain <lkh151515@gmail.com>
2020-04-25 03:21:18 +0900

    kh | make sevices reasonable

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { HomePage, AboutPage, LoginPage, TestPage, DetailPage, PerfumePage, SurveyPage, UserRank } from './pages'
-import storage from 'lib/storage';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as userActions from 'redux/modules/user';
-
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import {
+  HomePage,
+  AboutPage,
+  LoginPage,
+  DetailPage,
+  PerfumePage,
+  SurveyPage,
+  UserRank,
+} from "./pages";
+import storage from "lib/storage";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as userActions from "redux/modules/user";
 
 interface AppProps {
-  UserActions: any
+  UserActions: any;
 }
-
 
 class App extends Component<AppProps> {
   initializeUserInfo = async () => {
-    const loggedInfo = storage.get('loggedInfo'); // 로그인 정보를 로컬스토리지에서 가져옵니다.
+    const loggedInfo = storage.get("loggedInfo"); // 로그인 정보를 로컬스토리지에서 가져옵니다.
     if (!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
 
     const { UserActions } = this.props;
@@ -22,10 +28,10 @@ class App extends Component<AppProps> {
     try {
       await UserActions.checkStatus();
     } catch (e) {
-      storage.remove('loggedInfo');
-      window.location.href = '/auth/login?expired';
+      storage.remove("loggedInfo");
+      window.location.href = "/auth/login?expired";
     }
-  }
+  };
 
   componentDidMount() {
     this.initializeUserInfo();
@@ -36,20 +42,16 @@ class App extends Component<AppProps> {
       <div className="App">
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/test" component={TestPage} />
         <Route path="/perfume/" component={PerfumePage} />
         <Route path="/auth" component={LoginPage} />
         <Route path="/detail" component={DetailPage} />
         <Route path="/survey" component={SurveyPage} />
         <Route path="/rank" component={UserRank} />
       </div>
-    )
-  };
+    );
+  }
 }
 
-export default connect(
-  null,
-  (dispatch) => ({
-    UserActions: bindActionCreators(userActions, dispatch)
-  })
-)(App);
+export default connect(null, (dispatch) => ({
+  UserActions: bindActionCreators(userActions, dispatch),
+}))(App);
--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,75 +1,110 @@
-import React, {Component} from 'react'
-import { connect } from 'react-redux';
-import * as userActions from 'redux/modules/user';
-import { bindActionCreators } from 'redux';
-import storage from 'lib/storage';
-import 'materialize-css';
-import { Link, NavLink } from 'react-router-dom'
-import { Navbar, Icon } from 'react-materialize';
-import './Header.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as userActions from "redux/modules/user";
+import { bindActionCreators } from "redux";
+import storage from "lib/storage";
+import "materialize-css";
+import { Link, NavLink } from "react-router-dom";
+import { Navbar, Icon, Dropdown, Divider } from "react-materialize";
+import "./Header.css";
 
 interface HeaderProps {
-    UserActions: any,
-		user: any,
-    history: any,
+  UserActions: any;
+  user: any;
+  history: any;
 }
-class Header extends Component<HeaderProps>{
-
+class Header extends Component<HeaderProps> {
   handleLogout = async () => {
     const { UserActions, history } = this.props;
     try {
-        await UserActions.logout();
-				history.push('/')
+      await UserActions.logout();
+      history.push("/");
     } catch (e) {
-        console.log(e);
+      console.log(e);
     }
 
-    storage.remove('loggedInfo');
-    window.location.href = '/'; // 홈페이지로 새로고침
-}
+    storage.remove("loggedInfo");
+    window.location.href = "/"; // 홈페이지로 새로고침
+  };
 
- render() {
-   return(
-  <Navbar
-    alignLinks="right"
-    brand={<Link className="brand-logo" to="/">LAURE RICHIS</Link>}
-    id="mobile-nav"
-    menuIcon={<Icon>menu</Icon>}
-    options={{
-      draggable: false,
-      edge: 'left',
-      inDuration: 250,
-      onCloseEnd: null,
-      onCloseStart: null,
-      onOpenEnd: null,
-      onOpenStart: null,
-      outDuration: 200,
-      preventScrolling: true
-    }}
-  >
-    <NavLink to={`/perfume?page=1&sort=alpha&category=all&gender=all&include=all&exclude=None&brand=all`}>PERFUME</NavLink>
-    <Link to="/">NEWS</Link>
-    <Link to="/">ABOUT</Link>
-    <Link to="/survey">SURVEY</Link>
-		{ this.props.user.get('logged') 
-				? (<div onClick={this.handleLogout}>
-						{this.props.user.getIn(['loggedInfo', 'username'])} <span >(로그아웃)</span>
-				</div> )
-				: <Link to="/auth/login">
-      LOGIN
-    </Link>
-		}
-
-  </Navbar>
-);
- }
+  render() {
+    return (
+      <Navbar
+        alignLinks="right"
+        className="header-box"
+        brand={
+          <Link className="brand-logo" to="/">
+            LAURE RICHIS<small>로르 리시, 세상의 모든 향수</small>
+          </Link>
+        }
+        id="mobile-nav"
+        menuIcon={<Icon>menu</Icon>}
+        divider={true}
+        fixed={false}
+        options={{
+          draggable: false,
+          edge: "left",
+          inDuration: 300,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          outDuration: 200,
+          preventScrolling: false,
+        }}
+      >
+        <NavLink
+          to={`/perfume?page=1&sort=alpha&category=all&gender=all&include=all&exclude=None&brand=all`}
+        >
+          PERFUMES
+        </NavLink>
+        <Link to="/survey">SURVEY</Link>
+        <Link to="/">REVIEWS</Link>
+        <Link to="/">ABOUT</Link>
+        {this.props.user.get("logged") ? (
+          <Dropdown
+          id="dropdown_user"
+          options={{
+            alignment: 'right',
+            autoTrigger: true,
+            closeOnClick: true,
+            constrainWidth: true,
+            container: null,
+            coverTrigger: true,
+            hover: true,
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 250
+          }}
+          trigger={<div >{this.props.user.getIn(["loggedInfo", "username"])}</div>}
+        >
+          <Link style={{pointerEvents: "none"}} to="#" >
+            {this.props.user.getIn(["loggedInfo", "username"])}
+          </Link>
+          <Divider />
+          <Link to="#">124 points</Link>
+          
+          <Divider />
+          <Link to="#" onClick={this.handleLogout}>
+            LOGOUT
+          </Link>
+        </Dropdown>
+        ) : (
+          <Link to="/auth/login">LOGIN</Link>
+        )}
+      </Navbar>
+    );
+  }
 }
 
 export default connect(
-    (state) => ({
-        user: state.user
-    }),
-    (dispatch) => ({
-        UserActions: bindActionCreators(userActions, dispatch)
-    })
+  (state) => ({
+    user: state.user,
+  }),
+  (dispatch) => ({
+    UserActions: bindActionCreators(userActions, dispatch),
+  })
 )(Header);
