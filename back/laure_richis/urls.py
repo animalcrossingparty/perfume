1b315e332af7fd4a5c4ae1662f8e7baee26c24f4
HyejinYang <yang94lol@naver.com>
2020-04-26 22:39:34 +0900

    hj | FIX to edit swagger base url for production version

--- back/laure_richis/urls.py
+++ back/laure_richis/urls.py
@@ -5,6 +5,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from accounts.utils.EmailAuthBackend import EmailAuthBackend
+from django.conf import settings
 
 
 def home(requests):
@@ -18,11 +19,12 @@ schema_view = get_schema_view(
         description="로르리시 백엔드",
         contact=openapi.Contact(
             name="Hyejin Yang", email="yang94lol@naver.com", url="https://github.com/HyejinYang"),
-    ),
+        ),
     # authentication_classes=(EmailAuthBackend,),
     # validators=['ssv'],
     public=True,
     permission_classes=(permissions.AllowAny,),
+    url=settings.SWAGGER_BASE_URL,
 )
 
 urlpatterns = [
